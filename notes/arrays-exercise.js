3c6dad2925ebdcc75e6698bcdfa2a50f41d148e5
Reeyna <reeynarl@gmail.com>
2022-03-14 20:45:52 -0600

    [fix] exercise 3

--- notes/arrays-exercise.js
+++ notes/arrays-exercise.js
@@ -1,14 +1,13 @@
-var nums = []
-nums = [1,2,3,10,20,3,5,6,7,9]
-console.log(nums)
+// let nums = []
+// nums = [1,2,3,10,20,3,5,6,7,9]
+// console.log(nums)
 
-// var ingresar = prompt("ingresa palabras separadas por una coma")
-// var arreglo = ingresar.split(',')
+// let ingresar = prompt("ingresa números separados por una coma")
+// let arreglo = ingresar.split(',')
 // console.log(arreglo)
 
-var numbers = [10,40,30,20,15,5]
-var min = Math.min(...numbers)
-numbers.sort()
-numbers.pop()
-numbers.unshift(min)
-console.log(numbers)
+let numbers = [10,40,30,20,15,5]
+numbers.sort(function(a,b) {return a-b})
+let min = Math.min(...numbers) //devuelve el número menor de varios números
+let max = Math.max(...numbers)
+console.log(numbers + "\nNumero menor: "+min+"\nNúmero mayor: "+max)
